4c1a1b1ba9f50416840e05543a098ecbc2bbac38
HexValid <hexvalid@gmail.com>
2017-10-13 11:01:24 +0300

    Cleanup

--- scripts/fixheadphone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pulseaudio -k && sudo modprobe -rv snd-hda-intel &&  sudo modprobe -v snd-hda-intel patch=hda-jack-retask.fw,hda-jack-retask.fw,hda-jack-retask.fw,hda-jack-retask.fw 
-
-
